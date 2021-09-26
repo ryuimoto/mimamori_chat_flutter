4b554a32e3260acab9a2166e5e26cd0901f8dcc9
ryuimoto <narrow617@gmail.com>
2021-09-26 16:18:45 +0900

    news.dart add

---
+++ lib/user/routes/timeline.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Timeline extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.blueGrey[900],
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.orange,
+          title: Text('タイムライン'),
+        ),
+      ),
+    );
+  }
+}
